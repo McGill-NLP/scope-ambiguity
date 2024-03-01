3760d873f1b588486ffd3f9d8a9712eb55531ff7
Gaurav Kamath <gauravkamath1@gmail.com>
2024-03-01 15:07:03 -0500

    Update dataset_expansion.sh

--- scripts/dataset_expansion/dataset_expansion.sh
+++ scripts/dataset_expansion/dataset_expansion.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python 'exp1_dataset_expansion.py' --openai-api-key "<OPENAI_API_KEY>" --source-dataset-filepath "<filepath to Experiment 1A dataset -- see readme about access>" --generated-data-directory "<GENERATED_DATA_DIRECTORY>"
+python 'exp1_dataset_expansion.py' --openai-api-key "<OPENAI_API_KEY>" --source-dataset-filepath "<filepath to Experiment 1A dataset -- see README about access>" --generated-data-directory "<GENERATED_DATA_DIRECTORY>"
 python 'exp2_dataset_expansion.py' --openai-api-key "<OPENAI_API_KEY>" --source-dataset-filepath "datasets/exp2a_base_dataset.csv" --generated-data-directory "<GENERATED_DATA_DIRECTORY>"
